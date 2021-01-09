ee97b7251bcd70359acb30b0c25acb5ac3adbd08
Andrue Peters <5511409+AndruePeters@users.noreply.github.com>
2021-01-08 22:05:52 -0500

    compiling version of lv demo in my desired setup

--- app/main.cpp
+++ app/main.cpp
@@ -11,11 +11,17 @@
 
 /// Espressif Includes
 #include "driver/gpio.h"
-#include "driver/spi_common.h"
-#include "driver/spi_master.h"
+#include "esp_freertos_hooks.h"
+#include "esp_system.h"
 #include "freertos/FreeRTOS.h"
+#include "freertos/semphr.h"
 #include "freertos/task.h"
-#include "sdkconfig.h"
+
+/// External Projects
+#include <lvgl/lvgl.h>
+#include <lvgl_esp32_drivers/lvgl_helpers.h>
+#include "lv_examples/lv_ex_conf.h"
+#include "lv_examples/lv_examples/lv_examples.h"
 
 /// C++ Includes
 #include <algorithm>
@@ -26,65 +32,122 @@
 
 using namespace std::chrono_literals;
 
-/// Use GPIO pin 2 to blink the LED
-gpio_num_t BLINK_GPIO = static_cast<gpio_num_t>(2);
+/*********************
+ *      DEFINES
+ *********************/
+#define TAG "demo"
+#define LV_TICK_PERIOD_MS 1
 
+/**********************
+ *  STATIC PROTOTYPES
+ **********************/
+static void lv_tick_task(void *arg);
+static void guiTask(void *pvParameter);
+static void create_demo_application(void);
+
+/**********************
+ *   APPLICATION MAIN
+ **********************/
 /// app_main requires C bindings
 extern "C"
 {
     void app_main(void);
+    void lv_demo_widgets(void);
 }
 
-void app_main(void)
+void app_main()
 {
 
-    gpio_pad_select_gpio(BLINK_GPIO);
-    /* Set the GPIO as a push/pull output */
-    gpio_set_direction(BLINK_GPIO, GPIO_MODE_OUTPUT);
+    /* If you want to use a task to create the graphic, you NEED to create a Pinned task
+     * Otherwise there can be problem such as memory corruption and so on.
+     * NOTE: When not using Wi-Fi nor Bluetooth you can pin the guiTask to core 0 */
+    xTaskCreatePinnedToCore(guiTask, "gui", 4096 * 2, NULL, 0, NULL, 1);
+}
 
-    /*gpio_pad_select_gpio(GPIO_NUM_15);
-    gpio_set_direction(GPIO_NUM_15, GPIO_MODE_OUTPUT);
-    gpio_set_level(GPIO_NUM_15, 1);*/
+/* Creates a semaphore to handle concurrent call to lvgl stuff
+ * If you wish to call *any* lvgl function from other threads/tasks
+ * you should lock on the very same semaphore! */
+SemaphoreHandle_t xGuiSemaphore;
 
-    spi_bus_config_t buscfg = {
-        .mosi_io_num = 13,
-        .miso_io_num = 12,
-        .sclk_io_num = 14,
-        .quadwp_io_num = -1,
-        .quadhd_io_num = -1,
-        .max_transfer_sz = 32,
-    };
+static void guiTask(void *pvParameter)
+{
 
-    // Initialize the SPI bus
-    auto ret = spi_bus_initialize(HSPI_HOST, &buscfg, 0);
-    ESP_ERROR_CHECK(ret);
+    (void)pvParameter;
+    xGuiSemaphore = xSemaphoreCreateMutex();
 
-    auto devcfg = spi_device_interface_config_t{.command_bits = 0,
-                                                .address_bits = 0,
-                                                .dummy_bits = 0,
-                                                .mode = 1,
-                                                .duty_cycle_pos = 0,
-                                                .cs_ena_pretrans = 16,
-                                                .cs_ena_posttrans = 0,
-                                                .clock_speed_hz = 30'000,
-                                                .input_delay_ns = 0,
-                                                .spics_io_num = 15,
-                                                .flags = SPI_DEVICE_HALFDUPLEX,
-                                                .queue_size = 1,
-                                                .pre_cb = nullptr,
-                                                .post_cb = nullptr};
+    lv_init();
 
-    spi_device_handle_t spi;
-    ret = spi_bus_add_device(HSPI_HOST, &devcfg, &spi);
-    ESP_ERROR_CHECK(ret);
+    /* Initialize SPI or I2C bus used by the drivers */
+    lvgl_driver_init();
 
-    auto controller = max31865::Controller(devcfg, spi);
-    controller.writeRegister(max31865::register_write_address::configuration, 0b11010000);
+    static lv_color_t buf1[DISP_BUF_SIZE];
 
-    auto readConfig = controller.readRegister(max31865::register_read_address::configuration);
+    /* Use double buffered when not working with monochrome displays */
+#ifndef CONFIG_LV_TFT_DISPLAY_MONOCHROME
+    static lv_color_t buf2[DISP_BUF_SIZE];
+#else
+    static lv_color_t *buf2 = NULL;
+#endif
 
-    printf("\n\nOriginal: %d\n\n", 0b11010000);
-    printf("\n\nResult: %d\n\n", readConfig);
+    static lv_disp_buf_t disp_buf;
+
+    uint32_t size_in_px = DISP_BUF_SIZE;
+
+    /* Initialize the working buffer depending on the selected display.
+     * NOTE: buf2 == NULL when using monochrome displays. */
+    lv_disp_buf_init(&disp_buf, buf1, buf2, size_in_px);
+
+    lv_disp_drv_t disp_drv;
+    lv_disp_drv_init(&disp_drv);
+    disp_drv.flush_cb = disp_driver_flush;
+    disp_drv.buffer = &disp_buf;
+    lv_disp_drv_register(&disp_drv);
+
+    /* Create and start a periodic timer interrupt to call lv_tick_inc */
+    const esp_timer_create_args_t periodic_timer_args = {.callback = &lv_tick_task, .name = "periodic_gui"};
+    esp_timer_handle_t periodic_timer;
+    ESP_ERROR_CHECK(esp_timer_create(&periodic_timer_args, &periodic_timer));
+    ESP_ERROR_CHECK(esp_timer_start_periodic(periodic_timer, LV_TICK_PERIOD_MS * 1000));
+
+    /* Create the demo application */
+    create_demo_application();
+
+    while (1)
+    {
+        /* Delay 1 tick (assumes FreeRTOS tick is 10ms */
+        vTaskDelay(pdMS_TO_TICKS(10));
+
+        /* Try to take the semaphore, call lvgl related function on success */
+        if (pdTRUE == xSemaphoreTake(xGuiSemaphore, portMAX_DELAY))
+        {
+            lv_task_handler();
+            xSemaphoreGive(xGuiSemaphore);
+        }
+    }
+
+    /* A task should NEVER return */
+    vTaskDelete(NULL);
+}
+
+static void create_demo_application(void)
+{
+    /* When using a monochrome display we only show "Hello World" centered on the
+     * screen */
+    lv_demo_widgets();
+
+}
+
+static void lv_tick_task(void *arg)
+{
+    (void)arg;
+
+    lv_tick_inc(LV_TICK_PERIOD_MS);
+}
+/// Use GPIO pin 2 to blink the LED
+gpio_num_t BLINK_GPIO = static_cast<gpio_num_t>(2);
+
+/*void app_main(void)
+{
 
     /// Main while loop
     while (true)
@@ -92,4 +155,4 @@ void app_main(void)
 
         std::this_thread::sleep_for(1s);
     }
-}
+} */
